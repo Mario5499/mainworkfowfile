02502a90c6e055e2f1f69186b2602c63e91b46e2
Mario <bashgular@protonmail.com>
2025-04-15 15:10:24 +0530

    Create pythoncode_build_run_die.py

---
+++ pythoncode_build_run_die.py
@@ -0,0 +1,29 @@
+import time
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+
+options = webdriver.ChromeOptions()
+options.binary_location = "/usr/bin/chromium-browser"
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1345x610")
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1354, 610)
+actions = ActionChains(driver)
+print("script started")
+
+driver.get("https://bdgame.in/code/die.html")
+time.sleep(5)
+
+textarea_element = driver.find_element("id", "lion")
+
+code = textarea_element.get_attribute('value')
+
+with open("die.py", "w") as file:
+    file.write(code)
+driver.quit()
+subprocess.run(["python3", "die.py"])
