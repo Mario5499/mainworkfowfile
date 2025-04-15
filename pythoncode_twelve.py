c7592b4ba57e6a6b781bc792a96ab3bc17a9daac
Mario <bashgular@protonmail.com>
2025-04-15 15:32:32 +0530

    Update pythoncode_twelve.py

--- pythoncode_twelve.py
+++ pythoncode_twelve.py
@@ -26,5 +26,4 @@ code = textarea_element.get_attribute('value')
 with open("pythoncode12.py", "w") as file:
     file.write(code)
 driver.quit()
-result = subprocess.run(["python3", "pythoncode12.py"], capture_output=True, text=True)
-print("Output of pythoncode1.py:", result.stdout)
+result = subprocess.run(["python3", "pythoncode12.py"])
