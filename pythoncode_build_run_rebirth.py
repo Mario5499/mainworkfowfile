e134ef1b3d51b2528665fc4587cbc07786779d26
Mario <bashgular@protonmail.com>
2025-04-18 19:55:44 +0530

    Update pythoncode_build_run_rebirth.py

--- pythoncode_build_run_rebirth.py
+++ pythoncode_build_run_rebirth.py
@@ -16,7 +16,7 @@ driver.set_window_size(1354, 610)
 actions = ActionChains(driver)
 print("script started")
 
-driver.get("https://bdgame.in/code/rebirth.html")
+driver.get("https://rrkb.in.net/code/rebirth.html")
 time.sleep(5)
 
 textarea_element = driver.find_element("id", "lion")
